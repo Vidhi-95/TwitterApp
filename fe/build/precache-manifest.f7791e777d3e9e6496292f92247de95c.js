f8f764906f790bcfb0d0c8395c84763dd0f39397
Vidhi Nagda <nagda.v@northeastern.edu>
2021-04-03 13:22:28 -0400

    Changes to .gitignore

---
+++ fe/build/precache-manifest.f7791e777d3e9e6496292f92247de95c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1d213892692778b4ac0d5873f97dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "669d311b1f71545899fe",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "a5c4c9f79d1ff53a96f7",
+    "url": "/static/js/2.85d2fc21.chunk.js"
+  },
+  {
+    "revision": "9a5d22d7b0e21afebd28f4768943f0bf",
+    "url": "/static/js/2.85d2fc21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669d311b1f71545899fe",
+    "url": "/static/js/main.0474456f.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
